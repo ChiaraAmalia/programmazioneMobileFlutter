5e506b1a95806de09611019a387829fbef6c39cb
ChiaraAmalia <s1087171@studenti.univpm.it>
2021-06-15 22:42:57 +0200

    Revert "Update Dispensa.dart"
    This reverts commit a08436b8101ac122d76737ccf0eaed29a29dd62d.

--- easycookingflutter/lib/pages/Dispensa.dart
+++ easycookingflutter/lib/pages/Dispensa.dart
@@ -186,7 +186,7 @@ class _DispensaState extends State<Dispensa> {
     return Scaffold(
       body:Center(
         child:Column(
-        children:MediaQuery.of(context).orientation==Orientation.portrait ? <Widget>[
+        children:[
           Expanded(child: SizedBox(
         height:120.0,
         child: FutureBuilder(
@@ -321,166 +321,14 @@ class _DispensaState extends State<Dispensa> {
             icon: const Icon(Icons.add),
             backgroundColor: Colors.redAccent,
           ),
-      ] : <Widget>[
-        Column(
-          children:[
-          Row(
-            children: <Widget>[
-              SizedBox(
-            height:120.0,
-            width: 350.0,
-            child: FutureBuilder(
-              future: this.handler.retriveProdotti(),
-              builder: (BuildContext context, AsyncSnapshot<List<Prodotto>> snapshot) {
-                
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    /*
-                Se il DataBase contiene prodotti, mostra una lista di quest'ultimi, con la possibilità di
-                cancellarli scorrendo da destra a sinistra
-                */
-                    itemCount: snapshot.data?.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Dismissible(
-                        direction: DismissDirection.endToStart,
-                        background: Container(
-                          color: Colors.red,
-                          alignment: Alignment.centerRight,
-                          padding: EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Icon(Icons.delete_forever),
-                        ),
-                        key: ValueKey<String>(snapshot.data![index].nome_prodotto),
-                        onDismissed: (DismissDirection direction) async {
-                          await this.handler.cancellaProdotto(snapshot.data![index].nome_prodotto);
-                          setState(() {
-                            snapshot.data!.remove(snapshot.data![index]);
-                          });
-                        },
-                        child: Card(
-                          child: ListTile(
-                            contentPadding: EdgeInsets.all(8.0),
-                            title: Text(snapshot.data![index].nome_prodotto),
-                          ),
-                        ),
-                      );
-
-                    },
-                  );
-                  }
-                else {
-                  /*
-              Se lo snapshot non contiene dati, viene mostrato un indicatore di progresso circolare (rotellina che gira)
-               */
-                  return Center(child: CircularProgressIndicator());
-                }
-                },
-            ),
-              )]),
-        ],
-      ),
-    Container(
-    child: Row(
-    children: [
-          FloatingActionButton.extended(
-            /*
-            Bottone che se cliccato, cerca le ricette di Firebase che contengono tra gli ingredienti, gli
-             elementi presenti nella dispensa
-             */
-            onPressed: () async {
-              ingredientiFilter.clear();
-              var listaa = await this.handler.retriveProdotti();
-              ingredientiFilter.addAll(listaa);
-
-              //ricetteFilter = ricettaList;
-              for (var ric in ricettaList){
-                for (var ingr in ingredientiFilter){
-
-                  List<String> ingedients = [];
-                  for (var ingre in ric.ingredienti){
-
-                    ingedients.add(ingre.toString().toLowerCase());
-                  } if (ingedients.contains(ingr.nome_prodotto.toString().toLowerCase())){
-                    if(!(ricetteFilter.contains(ric))) {
-                      ricetteFilter.add(ric);
-                    }
-                  }
-                }
-              }
-
-              setState(() {
-                //
-              });
-            },
-            label: const Text('Cerca'),
-            icon: const Icon(Icons.search),
-            backgroundColor: Colors.redAccent,
-          )])),
-          SizedBox(height: 12.0),
-    Container(
-    child: Row(
-    children: [
-          FloatingActionButton.extended(
-            /*
-            Bottone che mostra l'Alert Dialog (descritto sopra), per aggiungere un prodotto
-            in dispensa
-             */
-            onPressed: () {
-              _displayTextInputDialog(context);
-            },
-            label: const Text('Aggiungi'),
-            icon: const Icon(Icons.add),
-            backgroundColor: Colors.redAccent,
-          )])),
-      Column(
-          children: <Widget>[
-          /*
-                  Lista che mostra, se esistono, le ricette presenti nel database di firebase,
-                  che tra i loro ingredienti hanno quelli in dispensa
-                   */
-          SizedBox(
-           height:120.0,
-           width: 200.0,
-        child: ListView.builder(
-                itemCount: ricetteFilter.length,
-                itemBuilder: (_, index) {
-                  var urlImage = "https://firebasestorage.googleapis.com/v0/b/gino-49a3d.appspot.com/o/images%2F" +
-                      ricetteFilter[index].image +
-                      "?alt=media&token=323e6eb7-b6e6-4b59-9ce8-f8936cf3cd29";
-                  return GestureDetector(
-                    /*
-                        Al clic sulla card relativa ad una specifica ricetta, ti rimanda a quella ricetta nel dettaglio
-                        dove sono presenti tutte le informazioni relative a quest'ultima
-                         */
-                      onTap: () async {
-                        List<String> manc=[];
-                        var lista=[];
-                        var ingre=ricetteFilter[index].ingredienti;
-                        var listaa = await this.handler.retriveProdotti();
-                        for ( var li in listaa){
-                          lista.add(li.nome_prodotto.toLowerCase());
-                        }
-
-                        for (var ing in ingre){
-                          if (!lista.contains(ing.toString().toLowerCase())){
-                            manc.add(ing.toString());
-                          }
-                        }
-                        _displaySpesaDialog(context, manc,index);
-
-                      },
-
-                      child: CardUI(ricetteFilter[index].nome, urlImage));
-                })),
-              ]
+      ]
+        ),
       ),
 
 
-        ]),
-      ),
     );
   }
 }
-
 Widget CardUI(String nome, String image){
   /*
   Card che viene mostrata nella ListView, e che contiene nome e foto delle singole ricette
